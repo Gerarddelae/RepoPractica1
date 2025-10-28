1e8469364e10f0ebd68b78e8ae508c9198597d05
Isaverg <vergaraisaias32@gmail.com>
2025-10-27 21:40:16 -0500

    agrege la clase estudiante

--- EjercicioJAVA/src/Estudiante.java
+++ EjercicioJAVA/src/Estudiante.java
@@ -15,4 +15,28 @@ public void mostrarDatos() {
     System.out.println("Edad: " + edad);
 }
 
+public String getNombre() {
+    return nombre;
+}
+
+public void setNombre(String nombre) {
+    this.nombre = nombre;
+}
+
+public String getApellido() {
+    return apellido;
+}
+
+public void setApellido(String apellido) {
+    this.apellido = apellido;
+}
+
+public int getEdad() {
+    return edad;
+}
+
+public void setEdad(int edad) {
+    this.edad = edad;
+}
+
 }
